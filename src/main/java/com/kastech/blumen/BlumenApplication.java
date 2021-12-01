3ec45200987673e265a94345499001175185fcd7
muhammedyounus <muhammedyounusattari@gmail.com>
2021-12-01 18:58:43 +0530

    Adding swagger ui dependency

--- src/main/java/com/kastech/blumen/BlumenApplication.java
+++ src/main/java/com/kastech/blumen/BlumenApplication.java
@@ -13,6 +13,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
