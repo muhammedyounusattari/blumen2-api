e6b0343fab626fd16732d7a49c827a2bc374d25b
Muhammed Younus <blumen.compansol@ktechproducts.com>
2021-12-17 11:45:37 +0530

    Code to move from azure local to github

---
+++ src/main/java/com/kastech/blumen/config/BlumenConfiguration.java
@@ -0,0 +1,20 @@
+package com.kastech.blumen.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class BlumenConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+       // registry.addMapping("/api/**");
+        //registry.addMapping("/**");
+    }
+}
