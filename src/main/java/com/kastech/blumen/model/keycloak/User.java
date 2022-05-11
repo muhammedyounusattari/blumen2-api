86d8dd0d58c1b8344f5e5461c39a9e8de17ab6b7
Muhammed Younus <blumen.compansol@ktechproducts.com>
2022-05-11 18:09:56 +0530

    code related to reset-password fixes

--- src/main/java/com/kastech/blumen/model/keycloak/User.java
+++ src/main/java/com/kastech/blumen/model/keycloak/User.java
@@ -45,7 +45,7 @@ public class User {
 
     public User() {}
 
-    public User(UserBuilder userBuilder) {
+    public User(User.UserBuilder userBuilder) {
         this.id = userBuilder.id;
         this.username = userBuilder.username;
         this.firstName = userBuilder.firstName;
@@ -69,37 +69,37 @@ public class User {
         public UserBuilder() {
         }
 
-        public UserBuilder id(String id) {
+        public User.UserBuilder id(String id) {
             this.id = id;
             return this;
         }
 
-        public UserBuilder username(String username) {
+        public User.UserBuilder username(String username) {
             this.username = username;
             return this;
         }
-        public UserBuilder firstName(String firstName) {
+        public User.UserBuilder firstName(String firstName) {
             this.firstName = firstName;
             return this;
         }
-        public UserBuilder lastName(String lastName) {
+        public User.UserBuilder lastName(String lastName) {
             this.lastName = lastName;
             return this;
         }
-        public UserBuilder email(String email) {
+        public User.UserBuilder email(String email) {
             this.email = email;
             return this;
         }
-        public UserBuilder enabled(boolean enabled) {
+        public User.UserBuilder enabled(boolean enabled) {
             this.enabled = enabled;
             return this;
         }
-        public UserBuilder emailVerified(boolean emailVerified) {
+        public User.UserBuilder emailVerified(boolean emailVerified) {
             this.emailVerified = emailVerified;
             return this;
         }
 
-        public UserBuilder credentials(List<Credentials> credentials) {
+        public User.UserBuilder credentials(List<Credentials> credentials) {
             this.credentials = credentials;
             return this;
         }
